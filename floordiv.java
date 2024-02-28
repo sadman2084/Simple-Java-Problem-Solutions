a97db6526459491e8f5b8d44a63206575613d360
Sadmankabir2084 <123572457+sadman2084@users.noreply.github.com>
2024-02-29 00:22:18 +0600

    new

---
+++ floordiv.java
@@ -0,0 +1,10 @@
+public class floordiv {
+    public static void main(String[] args) {
+        int x = -1234;
+        int y = 25;
+        System.out.println(x / y);
+        System.out.println(Math.floorDiv(x, y));
+        System.out.println(x % y);
+        System.out.println(Math.floorMod(x, y));
+    }
+}
